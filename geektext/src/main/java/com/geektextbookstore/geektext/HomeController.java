2f18e06b6f86ea4d17431e6dabf6d44ebe3878d5
server107 <71307167+server107@users.noreply.github.com>
2020-09-15 14:01:22 -0400

    Create HomeController.java

---
+++ geektext/src/main/java/com/geektextbookstore/geektext/HomeController.java
@@ -0,0 +1,14 @@
+package com.geektextbookstore.geektext;
+
+import org.springframework.stereotype.Controller;
+@Controller
+public class HomeController {
+
+@ReuestMapping
+public void home()
+	{
+    system.out.print()"Hello App");
+  }
+
+}
+
